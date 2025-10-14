8b39682cac46bada67322e530fd2dc8e7827936c
IvoBeltran <ivonnenatalylozada@gmail.com>
2025-10-14 15:45:02 -0500

    Cambie el nombre de la ruta de las imagenes, cree la carpeta mp3 con sus respectivos audios, cree  el archivo JSON canciones con sus datos

---
+++ js/main.js
@@ -0,0 +1,19 @@
+// Se declaran las variables globales
+const fondo = document.querySelector(".app");
+const imgCaratula = document.querySelector("#foto-caratula");
+const titulo = document.querySelector(".titulo-cancion");
+const artista = document.getElementById("artista");
+const btnAleatorio = document.getElementById("aleatorio");
+const btnAnterior = document.querySelector("#ant");
+const btnSiguiente = document.querySelector("#sig");
+const audio = document.getElementById("audio");
+const btnPlayPause = document.getElementById("play-pause");
+const barraProgress = document.getElementById("progress");
+const inicio = document.getElementById("inicio");
+const fin = document.getElementById("fin");
+const btnList = document.querySelector('#btn-lista');
+const contenedorCanciones = document.getElementById("contenedor-canciones");
+const btnRegreso = document.getElementById("btn-regreso");
+
+
+
